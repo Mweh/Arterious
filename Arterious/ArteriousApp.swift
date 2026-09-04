894396baffaf5098a59d2aaa55e301153a5fdbeb
fahmi <fahmianton@gmail.com>
2026-09-04 10:38:06 +0700

    Initial Commit

---
+++ Arterious/ArteriousApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArteriousApp.swift
+//  Arterious
+//
+//  Created by Muhammad Fahmi on 04/09/26.
+//
+
+import SwiftUI
+
+@main
+struct ArteriousApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
